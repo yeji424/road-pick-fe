09917dab63476c0f8911ad64f2c8d6471a6a6f63
yeji424 <yejiborn424@gmail.com>
2025-05-12 19:23:46 +0900

    Feat: 전체 레이아웃  수정

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,18 +4,19 @@ import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '@/components/common/Navbar/Navbar'
 import Footer from '@/components/common/Footer/Footer'
 import Spinner from '@/components/loading/Spinner'
-
 const MainLayout = () => {
   const location = useLocation()
   const isMapPage = location.pathname === '/map'
 
   return (
-    <div className={`layout-container ${isMapPage ? 'mapPage' : ''}`}>
+    <div className={`${isMapPage ? 'mapPage' : ''}`}>
       {!isMapPage && <Navbar />}
       <Suspense fallback={<Spinner />}>
         <Outlet />
       </Suspense>
       <Footer />
+
+      <Navbar />
     </div>
   )
 }
