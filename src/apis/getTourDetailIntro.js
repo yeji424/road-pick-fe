29529fe6155deec77dbe06aef83229e3235ecd2c
10012 <nas7062@naver.com>
2025-05-12 14:12:29 +0900

    feat : api함수 추가

---
+++ src/apis/getTourDetailIntro.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const SERVICE_KEY = import.meta.env.VITE_TOUR_API
+
+export const getTourDetailIntro = async (contentId, contentTypeId) => {
+  const response = await axios.get('https://apis.data.go.kr/B551011/KorService1/detailIntro1', {
+    params: {
+      MobileOS: 'ETC',
+      MobileApp: 'LoadPick',
+      _type: 'json',
+      contentTypeId,
+      contentId,
+      serviceKey: SERVICE_KEY,
+    },
+  })
+
+  const item = response.data.response.body.items.item
+
+  return item
+}
