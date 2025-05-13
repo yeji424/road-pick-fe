81b37bef1739a232b5d42537800b2cd668665516
10012 <nas7062@naver.com>
2025-05-14 00:14:54 +0900

    feat : ReactQuery 기본 설정

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client'
 import Router from './routes/AppRouter'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1, // 실패 시 1번만 재시도
+      refetchOnWindowFocus: false, // 창 포커스시 자동 리페치 비활성화
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </StrictMode>
 )
