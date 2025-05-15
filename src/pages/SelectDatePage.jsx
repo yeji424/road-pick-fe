acde062a5ba4c657932b8ed48993f05ecf982119
10012 <nas7062@naver.com>
2025-05-15 09:04:19 +0900

    feat : isOpen 시 모달 띄우기

--- src/pages/SelectDatePage.jsx
+++ src/pages/SelectDatePage.jsx
@@ -1,11 +1,19 @@
 import Calendar from '@/components/common/Calendar/Calendar'
 import { useState } from 'react'
 import css from './SelectDatePage.module.css'
-import DetailHeader from '@/components/detail/DetailHeader'
+import Header from '@/components/common/Header/Header'
+import RegistModal from '@/components/common/Modal/RegistModal'
 const SelectDatePage = () => {
   const [start, setStart] = useState('')
   const [end, setEnd] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
 
+  const ModalOpen = () => {
+    setIsOpen(true)
+  }
+  const ModalClose = () => {
+    setIsOpen(false)
+  }
   const SelectDate = date => {
     if (!start) {
       setStart(date)
@@ -23,7 +31,13 @@ const SelectDatePage = () => {
   }
   return (
     <main className={css.maincontainer}>
-      <DetailHeader />
+      <Header
+        showButton={true}
+        buttonText="일정 추가"
+        setIsOpen={setIsOpen}
+        onButtonClick={ModalOpen}
+      />
+      {isOpen && <RegistModal start={start} end={end} onClose={ModalClose} />}
       <h2>여행일정 등록</h2>
       <p>설레는 여행일정으로 달력을 채워보세요!</p>
       <Calendar SelectDate={SelectDate} start={start} end={end} />
