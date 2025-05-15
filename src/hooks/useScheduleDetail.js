e2d71da929972cc1f7819bbb55936af3aa469a41
10012 <nas7062@naver.com>
2025-05-15 15:41:51 +0900

    일정관련 api 사용 훅 구현

---
+++ src/hooks/useScheduleDetail.js
@@ -0,0 +1,33 @@
+import { getScheduleDetail } from '@/apis/scheduleApi'
+import { formatDateToLocalString } from '@/components/common/Calendar/CalendarLogic'
+import { useEffect, useState } from 'react'
+
+export const useScheduleDetail = scheduleId => {
+  const [schedule, setSchedule] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (!scheduleId) return
+    const fetchSheduleDetail = async () => {
+      try {
+        setLoading(true)
+        const data = await getScheduleDetail(scheduleId)
+        console.log(data)
+        const formattedData = {
+          ...data,
+          start: formatDateToLocalString(data.startDate),
+          end: formatDateToLocalString(data.endDate),
+        }
+        setSchedule(formattedData)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchSheduleDetail()
+  }, [scheduleId])
+
+  return { schedule, loading, error }
+}
