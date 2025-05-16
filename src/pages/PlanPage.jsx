bf98c8fb36b14d3fdb50a72e2a93e692b24b593e
10012 <nas7062@naver.com>
2025-05-16 16:30:58 +0900

    플랜 페이지 기능 추가

--- src/pages/PlanPage.jsx
+++ src/pages/PlanPage.jsx
@@ -9,18 +9,23 @@ import { useParams } from 'react-router-dom'
 import Spinner from '@/components/loading/Spinner'
 import { useScheduleDetail } from '@/hooks/useScheduleDetail'
 import MultiPlaceMap from '@/components/common/Map/PlanMap'
+import { formatDateToLocalString } from '@/components/common/Calendar/CalendarLogic'
+import { deleteScheduleDetail, getScheduleDetailByDate } from '@/apis/schedulsDetailApi'
+import { generateDateRange } from '@/components/common/BottomSheet/generateDateRange'
+import { useTourDetail } from '@/hooks/useTourDetail'
+import { useTourDetailCommons } from '@/hooks/useDetailsCommon'
 
 const PlanPage = () => {
   // 현재 화면 높이 기준으로 snap 위치 계산
   const screenHeight = typeof window !== 'undefined' ? window.innerHeight : 800
   const { tripId } = useParams()
   const { schedule, loading, error } = useScheduleDetail(tripId)
-  console.log(schedule)
-  if (!tripId) return <div>일정 ID 없음</div>
+  const [activitiesByDate, setActivitiesByDate] = useState({})
+
   const snapPoints = {
     full: 100, // 최상단
     mid: screenHeight * 0.6, // 중간
-    min: screenHeight * 0.4, // 초기 위치 (40%)
+    min: screenHeight * 0.2, // 초기 위치 (40%)
   }
 
   const [{ y }, api] = useSpring(() => ({
@@ -28,8 +33,70 @@ const PlanPage = () => {
     config: { tension: 300, friction: 30 },
   }))
 
-  if (loading) return <Spinner />
+  const fetchDetailPlan = async date => {
+    const formattedDate = formatDateToLocalString(date)
+    const response = await getScheduleDetailByDate(schedule.tripId, formattedDate)
+
+    setActivitiesByDate(prev => ({
+      ...prev,
+      [formattedDate]: response,
+    }))
+  }
+  const handleDeleteActivity = async (detailId, date) => {
+    try {
+      const formattedDate = formatDateToLocalString(date)
+      await deleteScheduleDetail(detailId, schedule.tripId, formattedDate)
+
+      setActivitiesByDate(prev => ({
+        ...prev,
+        [formattedDate]: prev[formattedDate].filter(activity => activity._id !== detailId),
+      }))
+    } catch (error) {
+      console.error('삭제 실패:', error)
+    }
+  }
+  const range = generateDateRange(schedule.start, schedule.end)
+  const selectedDate = range[0] // 예: Day 1만 먼저 표시
+  const selectedActivities = activitiesByDate[formatDateToLocalString(selectedDate)] || []
+  const tourItems = selectedActivities
+    .map(a => a.destination)
+    .filter(d => d?.contentid && d?.contenttypeid)
+    .map(d => ({
+      contentId: d.contentid,
+      contentTypeId: d.contenttypeid,
+    }))
+
+  const { data: commonDataList, isLoading } = useTourDetailCommons(tourItems)
+  const placeList = commonDataList.map((data, index) => {
+    const activity = selectedActivities[index]
+    if (!data) return
+    const dest = data[index]
+    console.log(dest)
+    if (!dest || !activity?._id) return null
+
+    const lat = parseFloat(dest.mapy)
+    const lng = parseFloat(dest.mapx)
+
+    if (isNaN(lat) || isNaN(lng)) return null
+
+    return {
+      id: activity._id,
+      name: dest.title,
+      lat,
+      lng,
+    }
+  })
+
+  console.log(placeList)
+  useEffect(() => {
+    if (schedule && range.length > 0) {
+      fetchDetailPlan(range[0])
+    }
+  }, [schedule])
+
+  if (loading || isLoading) return <Spinner />
   if (error) return <div>error...</div>
+
   return (
     <main>
       <Header />
@@ -58,16 +125,15 @@ const PlanPage = () => {
         </button>
       </section>
       <section>
-        <MultiPlaceMap
-          places={[
-            { id: 1, name: '서울역', lat: 37.554722, lng: 126.970833 },
-            { id: 2, name: '광화문', lat: 37.571389, lng: 126.976944 },
-            { id: 3, name: '강남역', lat: 37.497942, lng: 127.027621 },
-          ]}
-        />
+        <MultiPlaceMap places={placeList} />
       </section>
       <BottomSheet className={css.planPageBottomSheet} snapPoints={snapPoints} y={y} api={api}>
-        <BottomSheetPlan />
+        <BottomSheetPlan
+          schedule={schedule}
+          activitiesByDate={activitiesByDate}
+          fetchDetailPlan={fetchDetailPlan}
+          handleDeleteActivity={handleDeleteActivity}
+        />
       </BottomSheet>
     </main>
   )
