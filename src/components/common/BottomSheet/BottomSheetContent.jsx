662d0dcf81128d5c1c6fa2ad63fd7fa007a05a06
Hongjunior <seokjun09@gmail.com>
2025-05-16 16:28:45 +0900

    Design: 맵 바텀 시트 디자인 수정

--- src/components/common/BottomSheet/BottomSheetContent.jsx
+++ src/components/common/BottomSheet/BottomSheetContent.jsx
@@ -3,24 +3,32 @@ import css from './BottomSheet.module.css'
 import ListCard from '@/components/common/ListCard/ListCard'
 import SortDropdown from './SortDropdown'
 
-const BottomSheetContent = ({ list, setContentTypeId, onItemClick }) => {
+const BottomSheetContent = ({ list, contentTypeId, setContentTypeId, onItemClick }) => {
   const [sortBy, setSortBy] = useState('name')
 
   if (!list || list.length === 0) return <div>데이터 불러오는 중..</div>
 
+  const titleText = contentTypeId === 12 ? ' 근처 유명 관광지' : ' 근처 유명 축제/행사'
+
   return (
     <div className={css.contentWrapper}>
       <div className={css.tabWrapper}>
-        <button className={css.tabButton} onClick={() => setContentTypeId(12)}>
+        <button
+          className={`${css.tabButton} ${contentTypeId === 12 ? css.active : ''}`}
+          onClick={() => setContentTypeId(12)}
+        >
           관광지
         </button>
-        <button className={css.tabButton} onClick={() => setContentTypeId(15)}>
+        <button
+          className={`${css.tabButton} ${contentTypeId === 15 ? css.active : ''}`}
+          onClick={() => setContentTypeId(15)}
+        >
           축제/행사
         </button>
       </div>
 
       <div className={css.headerRow}>
-        <h2 className={css.title}>서울 유명 관광지</h2>
+        <h2 className={css.title}>{titleText}</h2>
         <SortDropdown sortBy={sortBy} onChange={setSortBy} />
       </div>
 
@@ -28,7 +36,6 @@ const BottomSheetContent = ({ list, setContentTypeId, onItemClick }) => {
         {list.map(item => (
           <div key={item.contentid} className={css.listItemWrapper}>
             <ListCard {...item} />
-            {/* 클릭 시 item 전체를 전달 */}
             <div className={css.itemOverlay} onClick={() => onItemClick(item)} />
           </div>
         ))}
