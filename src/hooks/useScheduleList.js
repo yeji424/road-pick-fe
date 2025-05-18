c3c241dce6ea413a60378604a1ab3c4c80d04b92
10012 <nas7062@naver.com>
2025-05-18 21:01:48 +0900

    feat : reactQuery 사용

--- src/hooks/useScheduleList.js
+++ src/hooks/useScheduleList.js
@@ -1,32 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
 import { getScheduleList } from '@/apis/scheduleApi'
 import { formatDateToLocalString } from '@/components/common/Calendar/CalendarLogic'
-import { useEffect, useState } from 'react'
 
-export const useScheduleList = () => {
-  const [schedules, setSchedules] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    const fetchSheduleList = async () => {
-      try {
-        setLoading(true)
-        const data = await getScheduleList()
-        const formattedData = data.map(schedule => ({
-          ...schedule,
-          start: formatDateToLocalString(schedule.startDate),
-          end: formatDateToLocalString(schedule.endDate),
-        }))
-        setSchedules(formattedData)
-      } catch (err) {
-        setError(err)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchSheduleList()
-  }, [])
-
-  return { schedules, loading, error }
+export const useScheduleList = userId => {
+  return useQuery({
+    queryKey: ['scheduleList', userId],
+    queryFn: async () => {
+      const data = await getScheduleList(userId)
+      return data.map(schedule => ({
+        ...schedule,
+        start: formatDateToLocalString(schedule.startDate),
+        end: formatDateToLocalString(schedule.endDate),
+      }))
+    },
+    enabled: !!userId,
+    staleTime: 5 * 60 * 1000,
+    retry: 1,
+  })
 }
