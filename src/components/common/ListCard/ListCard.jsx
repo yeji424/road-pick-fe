31410609a166cb3c3cd93dc23e4f4f65b381126f
KimJunSeo <rlawnstj901+2@gmail.com>
2025-05-19 14:41:51 +0900

    Fix: 지도페이지 디자인 수정 및 기능 수정

--- src/components/common/ListCard/ListCard.jsx
+++ src/components/common/ListCard/ListCard.jsx
@@ -34,6 +34,7 @@ const ListCard = ({
   mapy,
   onClick,
   isFavorite = false,
+  className = '',
 }) => {
   const navigate = useNavigate()
 
@@ -52,7 +53,7 @@ const ListCard = ({
   const randomReviewCount = getReviewCountFromId(contentid)
 
   return (
-    <div className={css.card} onClick={handleClick}>
+    <div className={`${css.card} ${className}`} onClick={handleClick}>
       <div className={css.thumbnail}>
         <img src={firstimage || noImage} alt={title} />
       </div>
