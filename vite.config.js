d6770ad9490655a336ff94e611be96e41324fe39
10012 <nas7062@naver.com>
2025-05-20 16:41:39 +0900

    vite 설정

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react(), svgr()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
