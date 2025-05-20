9d95b3d944b88af597b70363187c697f785ac0d3
10012 <nas7062@naver.com>
2025-05-20 15:32:09 +0900

    base url 변경

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), svgr()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
